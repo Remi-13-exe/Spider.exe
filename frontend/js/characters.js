05fab9761a8dd1c3ce0cba031f0d653beee86cce
Remi-13-exe <rgodji16@gmail.com>
2026-02-05 13:43:25 +0100

    MISE EN PLACE DE LA CARD

--- frontend/js/characters.js
+++ frontend/js/characters.js
@@ -10,3 +10,38 @@ filterBtn.addEventListener('click', () => {
     filtersPopup.style.display = 'flex';
   }
 });
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const grid = document.getElementById("characters-grid");
+
+  try {
+    const res = await fetch("http://localhost:3000/api/characters");
+    const characters = await res.json();
+
+    grid.innerHTML = "";
+
+    characters.forEach(char => {
+      const card = document.createElement("article");
+      card.classList.add("character-card");
+
+      card.innerHTML = `
+        <div class="character-img-wrapper">
+          <img src="${char.image_url}" alt="${char.name}">
+        </div>
+
+        <h2 class="character-name">${char.name}</h2>
+
+        <div class="character-meta">
+         
+          <span class="badge">${char.universe}</span>
+        </div>
+      `;
+
+      grid.appendChild(card);
+    });
+
+  } catch (err) {
+    console.error(err);
+    grid.innerHTML = `<p class="error">Erreur de chargement des personnages 🕷️</p>`;
+  }
+});
